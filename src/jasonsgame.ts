e6d87833cb10142280342b3967d09674994af960
Qihe(Jason)Wang <qwang122@ucsc.edu>
2024-10-08 15:50:19 -0700

    Step 1 complete

--- src/jasonsgame.ts
+++ src/jasonsgame.ts
@@ -1,5 +1,5 @@
 import "./style.css";
-
+// find the div with the id of app in html.
 const app: HTMLDivElement = document.querySelector("#app")!;
 
 const gameName = "My amazing game";
@@ -8,3 +8,12 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const button = document.createElement("button");
+button.innerHTML = "Click me!😈";
+
+button.onclick = () => {
+  alert("Button was clicked!");
+};
+
+app.append(button);
